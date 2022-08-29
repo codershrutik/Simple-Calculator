d0aa6d69d4fe515fa0e46716aab0052fbf9cc00a
codershrutik <72141758+codershrutik@users.noreply.github.com>
2022-08-29 22:16:10 +0530

    Add files via upload

---
+++ newcalc.py
@@ -0,0 +1,12 @@
+a = float(input("enter a: "))
+b = float(input("enter b: "))
+
+sum = a+b
+dif = a-b
+mult = a*b
+div = a/b
+
+print("the sum is ",sum)
+print("the difference is ",dif)
+print("the multiplication is",mult)
+print("the division is ",div)
